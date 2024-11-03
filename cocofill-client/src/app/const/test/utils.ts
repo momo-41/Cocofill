b4cafdbfe2569dfcef7e62fc7106e7c1198ae18e
momo-41 <minimizu41@gmail.com>
2024-11-03 16:35:16 +0900

    曜日を日本語表示にし、日曜始まりから月曜始まりに修正

--- cocofill-client/src/app/const/test/utils.ts
+++ cocofill-client/src/app/const/test/utils.ts
@@ -1,4 +1,7 @@
 import dayjs, { Dayjs } from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja"); //日本語表示
 
 export const hour = (keys: number) => [...Array(keys).keys()];
 
@@ -6,7 +9,10 @@ export function calcWeek(start?: string): Dayjs[] {
   //現在の週のデータを計算
   const date = start ? dayjs(start) : dayjs(); // 引数のstartに日付が入っていたらその日付を使ってdateに日付を返し、そうでない場合は現在の日付を返す
   const day = date.day(); // dateから曜日を取得(0[日]~6[土]で取得)
-  const startDay = date.subtract(day, "d"); // 日付からdayの数字分引いて日曜日の日付を返す
+
+  const startDay =
+    day === 0 ? date.subtract(6, "day") : date.subtract(day - 1, "day"); // 月曜始まりにするための調整
+
   const week: Dayjs[] = []; // Dayjs型の配列として初期化
   for (let day = 0; day < 7; day++) {
     week.push(startDay.add(day, "day")); // startDayに曜日の数字を足すことでその曜日と日付を取得している
