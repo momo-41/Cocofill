d21858f17c7c1f3143e38c86df1e1226a25963e1
momo-41 <minimizu41@gmail.com>
2024-11-09 17:27:51 +0900

    登録されている従業員の情報をIDも含めて表示

--- cocofill-client/app/page.tsx
+++ cocofill-client/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // import TestCalendarWeek from "./components/test/TestCalendarWeek";
 import TestTableMUI from "./components/test/TestTableMUI";
 import { Box } from "@mui/material";
-// import EmployeesView from "./components/EmployeesView";
+import EmployeesView from "./components/EmployeesView";
 // import TestTableSwitchCase from "./components/test/_TestTableSwitchCase";
 // import _TestTableRowSpan from "./components/test/_TestTableRowSpan";
 // import TestCalendar from "./components/test/_TestCalendar";
@@ -13,7 +13,7 @@ const Page = () => {
   return (
     <Box px={10}>
       {/* <InputEmployeeView /> */}
-      {/* <EmployeesView /> */}
+      <EmployeesView />
       {/* <TestCalendar /> */}
       {/* <TestDayjs /> */}
       {/* <TestTableSwitchCase /> */}
