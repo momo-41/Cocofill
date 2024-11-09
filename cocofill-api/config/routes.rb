7e6158571887af47582106cdf3ba08999cfd1e08
momo-41 <minimizu41@gmail.com>
2024-11-09 16:57:35 +0900

    ShiftSubmissionsのコントローラを作成

--- cocofill-api/config/routes.rb
+++ cocofill-api/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get "shift_submissions/index"
+      get "shift_submissions/show"
+      get "shift_submissions/create"
+      get "shift_submissions/update"
+      get "shift_submissions/destroy"
       resources :employees, only: [:index, :show, :create, :update, :destroy]
     end
   end
