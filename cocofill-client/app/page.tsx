c1dbc538ccbb3ff6d32ef2ee2bbc9a763f1b9d08
momo-41 <minimizu41@gmail.com>
2024-11-10 16:11:07 +0900

    不要なコンポーネントを削除

--- cocofill-client/app/page.tsx
+++ cocofill-client/app/page.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import TestTableMUI from "./_components/test/TestTableMUI";
 import { Box } from "@mui/material";
 // import EmployeesView from "./_components/EmployeesView";
-// import TestTableSwitchCase from "./_components/test/_TestTableSwitchCase";
-// import _TestTableRowSpan from "./_components/test/_TestTableRowSpan";
 // import TestCalendar from "./_components/test/_TestCalendar";
 // import TestDayjs from "./_components/test/_TestDayjs";
 
@@ -16,8 +14,6 @@ const Page = () => {
       {/* <EmployeesView /> */}
       {/* <TestCalendar /> */}
       {/* <TestDayjs /> */}
-      {/* <TestTableSwitchCase /> */}
-      {/* <TestTableRowSpan /> */}
       <TestTableMUI />
       {/* <TestCalendarWeek /> */}
     </Box>
