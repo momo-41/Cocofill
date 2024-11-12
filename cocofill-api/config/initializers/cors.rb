ce8a9113e82d43db966ab97072f4d93189f8ef6f
momo-41 <minimizu41@gmail.com>
2024-11-12 21:01:18 +0900

    cors.rbのURLを修正

--- cocofill-api/config/initializers/cors.rb
+++ cocofill-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "http://localhost:3000") # 環境変数からURLを読み込み、デフォルトでローカルを指定
+    origins "https://cocofill.onrender.com" # ポート番号の違うNext.js側のアクセスを許可する
 
     resource "*",
       headers: :any,
