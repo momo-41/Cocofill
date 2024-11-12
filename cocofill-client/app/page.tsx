9b02847e5838946141416eefb26125968ec2b0c9
momo-41 <minimizu41@gmail.com>
2024-11-12 18:59:01 +0900

    コメントアウトしているコードを削除

--- cocofill-client/app/page.tsx
+++ cocofill-client/app/page.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-// import InputEmployeeView from "./_components/InputEmployeeView";
-// import TestCalendarWeek from "./_components/test/TestCalendarWeek";
+
 import TestTableMUI from "./_components/test/TestTableMUI";
 import { Box } from "@mui/material";
-// import EmployeesView from "./_components/EmployeesView";
-// import TestCalendar from "./_components/test/_TestCalendar";
-// import TestDayjs from "./_components/test/_TestDayjs";
 
 const Page = () => {
   return (
     <Box px={10}>
-      {/* <InputEmployeeView /> */}
-      {/* <EmployeesView /> */}
-      {/* <TestCalendar /> */}
-      {/* <TestDayjs /> */}
       <TestTableMUI />
-      {/* <TestCalendarWeek /> */}
     </Box>
   );
 };
