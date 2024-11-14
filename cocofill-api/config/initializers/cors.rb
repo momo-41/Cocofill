76d5d7a9e832d2d494ee6cf01ba6e9694b8a4e7e
momo-41 <minimizu41@gmail.com>
2024-11-14 09:20:24 +0900

    cors.rbにローカルの番号も追加

--- cocofill-api/config/initializers/cors.rb
+++ cocofill-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://cocofill.onrender.com" # ポート番号の違うNext.js側のアクセスを許可する
+    origins "https://cocofill.onrender.com", 'http://localhost:3000' # ポート番号の違うNext.js側のアクセス(3000)を許可する
 
     resource "*",
       headers: :any,
