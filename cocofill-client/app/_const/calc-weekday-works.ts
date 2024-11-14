be78440b9b4d04909e295813d370d863bca89192
momo-41 <minimizu41@gmail.com>
2024-11-14 18:55:16 +0900

    平日の勤務日数を計算する関数と、休日の勤務日数を計算する関数を作成

--- cocofill-client/app/_const/calc-weekday-works.ts
+++ cocofill-client/app/_const/calc-weekday-works.ts
@@ -1,22 +1,21 @@
-export function calcWorkWeek(
+//平日の出勤回数計算
+export function calcWeekdayWorks(
   employeeName: string,
   startDate: string,
   endDate: string
 ): number {
   let count = 0;
-
-  // 開始日から終了日までの日付を生成して繰り返し処理
   const currentDate = new Date(startDate);
   const end = new Date(endDate);
 
-  //定義名をわかりやすく設定できるので、for文ではなくwhile文を採用
   while (currentDate <= end) {
-    const formattedDate = currentDate.toISOString().split("T")[0]; // "YYYY-MM-DD"形式に変換
+    const dayOfWeek = currentDate.getDay();
+    const formattedDate = currentDate.toISOString().split("T")[0];
     const key = `${employeeName}-${formattedDate}`;
     const value = localStorage.getItem(key);
 
-    // 「休」以外の値をカウント
-    if (value && value !== "休") {
+    // 平日の中で「休」以外の値をカウント
+    if (dayOfWeek >= 1 && dayOfWeek <= 5 && value && value !== "休") {
       count++;
     }
 
